c4f51ef227d13b3b8c16a7463592093f604a610a
sorensolari <sorensolari@gmail.com>
2018-11-30 16:08:44 -0800

    further bug fix

--- dnsscaling/ssl_credentials.py
+++ dnsscaling/ssl_credentials.py
@@ -135,7 +135,7 @@ class SslCredentials(object):
         # prep command for haproxy and make sure efs in sync
         self._execute_cmd(self._cat_copy_str(parenth=False))
         # stop haproxy
-        self._execute_cmd(self._stop_haproxy_str())
+        self._execute_cmd(self._stop_haproxy_str(parenth=False))
 
     def _execute_cmd(self, cmd):
 
